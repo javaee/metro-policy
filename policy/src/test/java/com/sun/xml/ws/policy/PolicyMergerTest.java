9e6548b9583744d131d8d88069d3db60102181d6
Fabian Ritzmann <ritzmann@java.net>
2009-03-06 14:52:07 +0000

    Test merging of policy assertions.
    svn path=/trunk/; revision=84

--- policy/src/test/java/com/sun/xml/ws/policy/PolicyMergerTest.java
+++ policy/src/test/java/com/sun/xml/ws/policy/PolicyMergerTest.java
@@ -178,6 +178,67 @@ public class PolicyMergerTest extends TestCase {
     /**
      * Test of merge method, of class PolicyMerger.
      */
+    public void testMergeTwo2() {
+        AssertionData assertionData1 = AssertionData.createAssertionData(new QName("http://example.test/", "Assertion1"));
+        AssertionData assertionData2 = AssertionData.createAssertionData(new QName("http://example.test/", "Assertion2"));
+        AssertionData assertionData3 = AssertionData.createAssertionData(new QName("http://example.test/", "Assertion3"));
+        AssertionData assertionData4 = AssertionData.createAssertionData(new QName("http://example.test/", "Assertion4"));
+        PolicyAssertion assertion1 = new PolicyAssertion(assertionData1, null) { };
+        PolicyAssertion assertion2 = new PolicyAssertion(assertionData2, null) { };
+        PolicyAssertion assertion3 = new PolicyAssertion(assertionData3, null) { };
+        PolicyAssertion assertion4 = new PolicyAssertion(assertionData4, null) { };
+        Collection<PolicyAssertion> assertions1 = new LinkedList<PolicyAssertion>();
+        assertions1.add(assertion1);
+        assertions1.add(assertion2);
+        assertions1.add(assertion3);
+        assertions1.add(assertion4);
+        AssertionSet assertionSet1 = AssertionSet.createAssertionSet(assertions1);
+        Collection<AssertionSet> assertionSets1 = new LinkedList<AssertionSet>();
+        assertionSets1.add(assertionSet1);
+        Policy policy1 = Policy.createPolicy(assertionSets1);
+
+        AssertionData assertionData23 = AssertionData.createAssertionData(new QName("http://example.test/", "Assertion3"));
+        AssertionData assertionData24 = AssertionData.createAssertionData(new QName("http://example.test/", "Assertion4"));
+        AssertionData assertionData25 = AssertionData.createAssertionData(new QName("http://example.test/", "Assertion5"));
+        AssertionData assertionData26 = AssertionData.createAssertionData(new QName("http://example.test/", "Assertion6"));
+        PolicyAssertion assertion23 = new PolicyAssertion(assertionData23, null) { };
+        PolicyAssertion assertion24 = new PolicyAssertion(assertionData24, null) { };
+        PolicyAssertion assertion25 = new PolicyAssertion(assertionData25, null) { };
+        PolicyAssertion assertion26 = new PolicyAssertion(assertionData26, null) { };
+        Collection<PolicyAssertion> assertions2 = new LinkedList<PolicyAssertion>();
+        assertions2.add(assertion23);
+        assertions2.add(assertion24);
+        assertions2.add(assertion25);
+        assertions2.add(assertion26);
+        AssertionSet assertionSet2 = AssertionSet.createAssertionSet(assertions2);
+        Collection<AssertionSet> assertionSets2 = new LinkedList<AssertionSet>();
+        assertionSets2.add(assertionSet2);
+        Policy policy2 = Policy.createPolicy(assertionSets2);
+
+        final Collection<Policy> policies = new LinkedList<Policy>();
+        policies.add(policy1);
+        policies.add(policy2);
+
+        Collection<PolicyAssertion> assertionsExp = new LinkedList<PolicyAssertion>();
+        assertionsExp.add(assertion1);
+        assertionsExp.add(assertion2);
+        assertionsExp.add(assertion3);
+        assertionsExp.add(assertion4);
+        assertionsExp.add(assertion25);
+        assertionsExp.add(assertion26);
+        AssertionSet assertionSetExp = AssertionSet.createAssertionSet(assertionsExp);
+        Collection<AssertionSet> assertionSetsExp = new LinkedList<AssertionSet>();
+        assertionSetsExp.add(assertionSetExp);
+        Policy expResult = Policy.createPolicy(assertionSetsExp);
+
+        final PolicyMerger instance = PolicyMerger.getMerger();
+        final Policy result = instance.merge(policies);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of merge method, of class PolicyMerger.
+     */
     public void testMergeId() {
         AssertionData assertionData1 = AssertionData.createAssertionData(new QName("http://example.test/", "Assertion1"));
         PolicyAssertion assertion1 = new PolicyAssertion(assertionData1, null) { };
