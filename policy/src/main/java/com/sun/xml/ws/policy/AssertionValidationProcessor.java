66ff8e0f67141d6ef6388dcf6772c53d78452104
Fabian Ritzmann <ritzmann@java.net>
2009-08-21 13:04:11 +0000

    Enable JAX-WS to pass in a pre-defined set of PolicyAssertionValidators.
    svn path=/trunk/; revision=112

--- policy/src/main/java/com/sun/xml/ws/policy/AssertionValidationProcessor.java
+++ policy/src/main/java/com/sun/xml/ws/policy/AssertionValidationProcessor.java
@@ -38,41 +38,69 @@ package com.sun.xml.ws.policy;
 import com.sun.xml.ws.policy.privateutil.PolicyLogger;
 import com.sun.xml.ws.policy.privateutil.PolicyUtils;
 import com.sun.xml.ws.policy.spi.PolicyAssertionValidator;
-
 import static com.sun.xml.ws.policy.privateutil.LocalizationMessages.WSP_0076_NO_SERVICE_PROVIDERS_FOUND;
 
+import java.util.Collection;
+import java.util.LinkedList;
+
 /**
- * Singleton class that provides method for assertion validation.
+ * Provides methods for assertion validation.
  *
  * @author Marek Potociar (marek.potociar at sun.com)
+ * @author Fabian Ritzmann
  */
-public final class AssertionValidationProcessor {
+public class AssertionValidationProcessor {
     private static final PolicyLogger LOGGER = PolicyLogger.getLogger(AssertionValidationProcessor.class);
     
-    private static AssertionValidationProcessor processor = new AssertionValidationProcessor();
-    private static final PolicyAssertionValidator[] validators;
-    static {
-        validators = PolicyUtils.ServiceProvider.load(PolicyAssertionValidator.class);
-    }
+    private final Collection<PolicyAssertionValidator> validators = new LinkedList<PolicyAssertionValidator>();
     
     /**
-     * This private constructor prevents direct instantiation of this class.
+     * This constructor instantiates the object with a set of dynamically
+     * discovered PolicyAssertionValidators.
+     *
+     * @throws PolicyException Thrown if the set of dynamically discovered
+     *   PolicyAssertionValidators is empty.
      */
-    private AssertionValidationProcessor() {
-        // no instantiation outside the class.
+    private AssertionValidationProcessor() throws PolicyException {
+        this(null);
     }
-    
+
+    /**
+     * This constructor adds the given set of policy validators to the dynamically
+     * discovered PolicyAssertionValidators.
+     *
+     * This constructor is intended to be used by the JAX-WS com.sun.xml.ws.policy.api.ValidationProcessor.
+     *
+     * @param policyValidators A set of PolicyAssertionValidators. May be null
+     * @throws PolicyException Thrown if the set of given PolicyAssertionValidators
+     *   and dynamically discovered PolicyAssertionValidators is empty.
+     */
+    protected AssertionValidationProcessor(final Collection<PolicyAssertionValidator> policyValidators)
+            throws PolicyException {
+        for(PolicyAssertionValidator validator : PolicyUtils.ServiceProvider.load(PolicyAssertionValidator.class)) {
+            validators.add(validator);
+        }
+        if (policyValidators != null) {
+            for (PolicyAssertionValidator validator : policyValidators) {
+                validators.add(validator);
+            }
+        }
+        if (validators.size() == 0) {
+            throw LOGGER.logSevereException(new PolicyException(WSP_0076_NO_SERVICE_PROVIDERS_FOUND(PolicyAssertionValidator.class.getName())));
+        }
+    }
+
     /**
      * Factory method that returns singleton instance of the class.
      * 
+     * This method is only intended to be used by code that has no dependencies on
+     * JAX-WS. Otherwise use com.sun.xml.ws.api.policy.ValidationProcessor.
+     *
      * @return singleton An instance of the class.
      * @throws PolicyException If instantiation failed.
      */
     public static AssertionValidationProcessor getInstance() throws PolicyException {
-        if (validators.length == 0) {
-            throw LOGGER.logSevereException(new PolicyException(WSP_0076_NO_SERVICE_PROVIDERS_FOUND(PolicyAssertionValidator.class.getName())));
-        }
-        return processor;
+        return new AssertionValidationProcessor();
     }
     
     /**
